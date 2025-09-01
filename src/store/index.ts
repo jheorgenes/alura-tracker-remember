4b60ef5f49e118f2fd37dd903125501fa17caab3
Jheorgenes Warlley <jheorgenes@gmail.com>
2025-09-01 01:39:22 -0300

    - Implementando CRUD com requisições da API nas Tarefas. - Fazendo o mesmo processo usando actions e mutations do Vuex que foi realizado em projetos.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,10 @@ import { INotificacao } from "@/interfaces/INotificacao";
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO } from "./tipo-acoes";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { ALTERAR_PROJETO, ALTERAR_TAREFA, CADASTRAR_PROJETO, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipo-acoes";
+import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import http from "@/http"
+import ITarefa from "@/interfaces/ITarefa";
 
 /**
  * Definição do estado global da aplicação
@@ -12,7 +13,8 @@ import http from "@/http"
  *   gerenciados pelo Vuex.
  */
 interface Estado {
-  projetos: IProjeto[]
+  projetos: IProjeto[],
+  tarefas: ITarefa[],
   notificacoes: INotificacao[]
 }
 
@@ -29,9 +31,11 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
   state: {
     projetos: [],
+    tarefas: [],
     notificacoes: []
   },
   mutations: {
+    // Projetos
     [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
       const projeto = {
         id: new Date().toISOString(),
@@ -49,6 +53,18 @@ export const store = createStore<Estado>({
     [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
       state.projetos = projetos; //Inserindo na lista os projetos obtidos do axios
     },
+    // Tarefas
+    [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
+      state.tarefas = tarefas; //Inserindo na lista os projetos obtidos do axios
+    },
+    [ADICIONA_TAREFA](state, tarefa: ITarefa) {
+      state.tarefas.push(tarefa);
+    },
+    [ALTERA_TAREFA](state, tarefa: ITarefa) {
+      const index = state.tarefas.findIndex(t => t.id === tarefa.id);
+      state.tarefas[index] = tarefa;
+    },
+    // Notificações
     [NOTIFICAR](state, novaNotificacao: INotificacao) {
       novaNotificacao.id = new Date().getTime();
       state.notificacoes.push(novaNotificacao);
@@ -60,6 +76,7 @@ export const store = createStore<Estado>({
     }
   },
   actions: {
+    // Projetos
     [OBTER_PROJETOS]({ commit }) { //Extraíndo apenas o método commit dos parametros disponíveis das mutations
       http.get('projetos')
           .then(resposta => commit(DEFINIR_PROJETOS, resposta.data)); //Chamando a mutation para injetar a lista de projetos recuperada
@@ -75,7 +92,20 @@ export const store = createStore<Estado>({
     [REMOVER_PROJETO]({ commit }, id: string) { //Esse commit reaproveita o estado local para disparar uma nova mutation
       return http.delete(`/projetos/${id}`)
         .then(() => commit(EXCLUIR_PROJETO, id));
-    }
+    },
+    // Tarefas
+    [OBTER_TAREFAS]({ commit }) {
+      http.get('tarefas')
+          .then(resposta => commit(DEFINIR_TAREFAS, resposta.data));
+    },
+    [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
+      return http.post('/tarefas', tarefa)
+        .then(resposta => commit(ADICIONA_TAREFA, resposta.data));
+    },
+    [ALTERAR_TAREFA]({ commit }, tarefa: ITarefa) {
+      return http.put(`/tarefas/${tarefa.id}`, tarefa)
+        .then(() => commit(ALTERA_TAREFA, tarefa));
+    },
   }
 });
 
